6d5aa53e77035fe8a6f8e71c22c7a9cf6fd65679
matt lubel <matt_lubel@hotmail.com>
2018-11-20 09:59:59 +0200

    simplify keybaord (remove keys etc)

--- src/Key.js
+++ src/Key.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react'
 import { keyMap } from './consts'
 
 const Key = forwardRef(({ character }, ref) => (
-  <div id={`key-${character}`} className="key" ref={ref}>
+  <div className="key" ref={ref}>
     <span className="english-character">
       {character}
     </span>
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -44,55 +44,24 @@ class Keyboard extends Component {
     document.removeEventListener('keyup')
   }
 
+  renderKey(key) {
+    return (
+      <Key character={key} key={key} ref={this.setRef(key)} />
+    )
+  }
+
   render() {
     return (
       <div className="container">
         <div className="keyboard">
-          <div className="keyboard-row">
-            <div className="key extra-size lowercase lower-left unused">
-              <span className="lower-row-text">tab</span>
-            </div>
-            {keys[0].map(key => (
-              <Key character={key} key={key} ref={this.setRef(key)} />
-            ))}
-            <div className="key unused"><span></span></div>
+          <div className="keyboard-row row-1">
+            {keys[0].map(key => this.renderKey(key))}
           </div>
-          <div className="keyboard-row">
-            <div className="key lowercase lower-left extra-size-two unused"><span className="lower-row-text">caps lock</span><span className="absolute-left caps-dot">&bull;</span></div>
-            {keys[1].map(key => (
-              <Key character={key} key={key} ref={this.setRef(key)} />
-            ))}
-            <div className="key unused"><span></span></div>
-            <div className="key extra-size-two lowercase lower-right unused">
-              <span className="lower-row-text">return</span>
-            </div>
+          <div className="keyboard-row row-2">
+            {keys[1].map(key => this.renderKey(key))}
           </div>
-          <div className="keyboard-row">
-            <div className="key double-size lowercase lower-left unused"><span className="lower-row-text">shift</span></div>
-            {keys[2].map(key => (
-              <Key character={key} key={key} ref={this.setRef(key)} />
-            ))}
-            <div className="key unused"><span></span></div>
-            <div className="key unused"><span></span></div>
-            <div className="key unused"><span></span></div>
-            <div className="key double-size lowercase lower-right unused"><span className="lower-row-text">shift</span></div>
-          </div>
-          <div className="keyboard-row bottom-row">
-            <div className="key unused lower-left lowercase"><span className="lower-row-text">fn</span></div>
-            <div className="key unused lower-left lowercase"><span className="lower-row-text">control</span></div>
-            <div className="key unused lower-left lowercase"><span className="lower-row-text">option</span><span className="absolute-left">alt</span></div>
-            <div className="key unused lower-center lowercase extra-size-two"><span className="lower-row-text">command</span><span className="absolute-right icon">&#8984;</span></div>
-            <div className="key space-bar"></div>
-            <div className="key unused lower-center lowercase extra-size-two"><span className="lower-row-text">command</span><span className="absolute-left icon">&#8984;</span></div>
-            <div className="key unused lower-right lowercase"><span className="lower-row-text">option</span><span className="absolute-right">alt</span></div>
-            <div className="arrows">
-              <div className="key unused">&#9668;</div>
-              <div className="up-down">
-                <div className="key unused">&#9650;</div>
-                <div className="key unused">&#9660;</div>
-              </div>
-              <div className="key unused">&#9658;</div>
-            </div>
+          <div className="keyboard-row row-3">
+            {keys[2].map(key => this.renderKey(key))}
           </div>
         </div>
       </div>
