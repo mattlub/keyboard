f844631a2f35c15a8047b9cb66dfb849605e4c1a
matt lubel <matt_lubel@hotmail.com>
2019-01-15 12:18:38 +0100

    add show answer button

--- src/Test.js
+++ src/Test.js
@@ -9,13 +9,15 @@ const NEXT_WORD_DELAY = 2000
 const INITIAL_STATE = {
   correct: false,
   word: pickRandom(words),
-  input: ''
+  input: '',
+  showAnswer: false
 }
 
 class Test extends Component {
   constructor(props) {
     super(props)
     this.state = INITIAL_STATE
+    this.inputRef = null
   }
 
   handleInputChange(e) {
@@ -36,7 +38,8 @@ class Test extends Component {
         this.setState(prevState => ({
           correct: false,
           word: pickRandom(words, [prevState.word]),
-          input: ''
+          input: '',
+          showAnswer: false
         }))
       }, NEXT_WORD_DELAY)
     }
@@ -45,12 +48,20 @@ class Test extends Component {
   handleSkip() {
     this.setState(prevState => ({
       word: pickRandom(words, [prevState.word]),
-      input: ''
+      input: '',
+      showAnswer: false
     }))
   }
 
+  handleShowAnswer() {
+    this.setState({
+      showAnswer: true
+    })
+    this.inputRef.focus()
+  }
+
   render() {
-    const { word, correct, input } = this.state
+    const { word, correct, input, showAnswer } = this.state
     return (
       <div>
         <div className="guess-section" >
@@ -58,14 +69,24 @@ class Test extends Component {
             {word.en}
           </div>
           <input
+            ref={e => this.inputRef = e}
             className={`arabic-input ${correct ? 'correct' : ''}`}
             direction="rtl"
             value={input}
             onChange={e => this.handleInputChange(e)}
           />
+          <div className="show-answer">
+            {showAnswer && word.ar}
+          </div>
           <div>
             <button onClick={() => this.handleSkip()}>
               Skip
+            </button>
+            <button
+              disabled={showAnswer}
+              onClick={() => this.handleShowAnswer()}
+            >
+              Show Answer
             </button>
           </div>
         </div>
